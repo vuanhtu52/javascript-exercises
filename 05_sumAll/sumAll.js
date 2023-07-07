066825201e6bbcdd122426f90c9089aa9b7ea2a5
Tu Vu <vuanhtu52@gmail.com>
2023-07-07 17:23:54 +0700

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let min;
+    let max;
+    // Return ERROR with non-number params
+    if (typeof(num1) !== "number" || typeof(num2) !== "number") {
+        return "ERROR";
+    }
+    // Return ERROR with negative numbers
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    // Check which number is smaller/larger
+    if (num1 <= num2) {
+        min = num1;
+        max = num2;
+    } else {
+        min = num2;
+        max = num1;
+    }
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
