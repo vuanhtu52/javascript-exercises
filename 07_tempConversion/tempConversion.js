a477fc54d614811ef7b355dc4d675f94b0206de4
Tu Vu <vuanhtu52@gmail.com>
2023-07-08 00:01:47 +0700

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fValue) {
+  cValue = 5 / 9 * (fValue - 32);
+  cValue = Math.round(cValue * 10) / 10
+  return cValue;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cValue) {
+  fValue = (cValue * 9 / 5) + 32;
+  fValue = Math.round(fValue * 10) / 10;
+  return fValue;
 };
 
 // Do not edit below this line
