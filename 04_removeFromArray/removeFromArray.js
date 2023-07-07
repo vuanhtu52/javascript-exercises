b0da947e0ffee09c2298bb790639be129834368c
Tu Vu <vuanhtu52@gmail.com>
2023-07-07 17:03:12 +0700

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    for (let arg of args) {
+        index = arr.indexOf(arg);
+        if (index >= 0) {
+            arr.splice(index, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
