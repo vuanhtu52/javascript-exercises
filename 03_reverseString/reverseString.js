484c2e40852863406447b5fa19048744da9a284c
Tu Vu <vuanhtu52@gmail.com>
2023-07-07 15:45:02 +0700

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringReverse = "";
+    for (let i = string.length; i >= 0; i--) {
+        stringReverse += string.charAt(i);
+    }
+    return stringReverse;
 };
 
 // Do not edit below this line
