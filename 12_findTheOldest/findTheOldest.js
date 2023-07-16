cc9c500589af5857465d86ceb197e1cb1f57f2c8
Tu Vu <vuanhtu52@gmail.com>
2023-07-16 16:59:04 +0700

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const sortedPeople = people.sort((person1, person2) => {
+        if (!("yearOfDeath" in person1)) {
+            person1.yearOfDeath = (new Date()).getFullYear();
+        }
+        if (!("yearOfDeath" in person2)) {
+            person2.yearOfDeath = (new Date()).getFullYear();
+        }
+        return (person1.yearOfDeath - person1.yearOfBirth) - (person2.yearOfDeath - person2.yearOfBirth);
+    });
+    console.log(sortedPeople);
+    return sortedPeople[sortedPeople.length - 1];
 };
 
 // Do not edit below this line
