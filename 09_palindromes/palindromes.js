144a86661b3ee158f3f99e3bc9a4f7c45f47c6c0
Tu Vu <vuanhtu52@gmail.com>
2023-07-16 15:25:20 +0700

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    str = str.replace(/\s/g, "");
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] !== str[str.length - 1 - i]) {
+            return false
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
