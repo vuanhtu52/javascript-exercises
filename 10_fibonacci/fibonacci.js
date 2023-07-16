f8f75c8461a0d0de7bc6f845fb032023179ecb58
Tu Vu <vuanhtu52@gmail.com>
2023-07-16 15:56:52 +0700

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    index = parseInt(index);
+    if (index < 0) {
+        return "OOPS";
+    }
+    if (index === 1 || index === 2) {
+        return 1;
+    }
+    let num1 = 1;
+    let num2 = 1;
+    let result;
+    for (let i = 3; i <= index; i++) {
+        result = num1 + num2;
+        num1 = num2;
+        num2 = result;
+    }
+    return result;
 };
 
 // Do not edit below this line
