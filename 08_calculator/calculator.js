a95eeb16ca36a8047304016393aef5f31ef61769
Tu Vu <vuanhtu52@gmail.com>
2023-07-16 15:02:28 +0700

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+ return array.reduce((total, num) => total += num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  return args.reduce((product, num) => product *= num, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num < 0) {
+    return "ERR";
+  }
+  if (num === 0) {
+    return 1;
+  }
+  result = 1;
+  for (let i = 1; i <= num; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
